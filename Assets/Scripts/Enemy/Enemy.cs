de3e4d8e44cce7f72bdf8238504831d5aae19009
Trenton <tregilbe@uat.edu>
2020-04-07 15:23:25 -0500

    Add Enemy Patrol behavior, and started work on health bars for enemies

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,42 +7,97 @@ public class Enemy : MonoBehaviour
     public float speed;
     public float stoppingDistance;
     public float retreatDistance;
+    public float waitTime;
+    public float startWaitTime;
 
     public Transform player;
     public Rigidbody2D rb;
     private Animator animator;
 
-    
+    public Transform[] moveSpots;
+    private int randomSpot;
+
+    public bool isPatrolling = false;
+    public bool isChasing = false;
 
     public int health;
 
     void Start()
     {
-
         rb = GetComponent<Rigidbody2D>();
         animator = gameObject.GetComponent<Animator>();
         GameManager.instance.enemiesList.Add(this.gameObject);
+
+        waitTime = startWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
     }
 
     private void Update()
     {
+        // Defines target
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        // Tracks health, destroys if 0
         if (health <= 0)
         {
             Destroy(gameObject);
         }
 
-        if(Vector2.Distance(transform.position, player.position) > stoppingDistance)
+        if (isPatrolling == true)
+        {
+            Patrol();
+        }else if (isChasing == true)
+        {
+            Chase();
+        }
+        else
+        {
+            Idle();
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+    void OnDestroy()
+    {
+        GameManager.instance.enemiesList.Remove(this.gameObject);
+    }
+    void Patrol()
+    {
+        //Patrol
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+
+    }
+    void Chase()
+    {
+        // Chase and attack
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-        }else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance)
         {
             transform.position = this.transform.position;
-        }else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
         }
-    
+
         if (transform.position.x < player.position.x)
         {
             animator.Play("WarriorWalkRight");
@@ -64,13 +119,8 @@ public class Enemy : MonoBehaviour
             animator.Play("WarriorIdle");
         }
     }
-
-    public void TakeDamage(int damage)
+    void Idle()
     {
-        health -= damage;
-    }
-    void OnDestroy()
-    {
-        GameManager.instance.enemiesList.Remove(this.gameObject);
+        animator.Play("WarriorIdle");
     }
 }
